1d49a3928b3ebc197351b7373f02f8f27e61b5fa
Filipe Oliveira <contato@fmoliveira.com.br>
2016-03-14 23:46:22 -0300

    Enhanced sites model with SSL and FQDNs.

--- models/site.js
+++ models/site.js
@@ -6,15 +6,49 @@ var SiteSchema = new mongoose.Schema({
   // Basic info
   name: { type: String, required: true },
 
+  // Date events
+  created_time: Date,
+  updated_time: Date,
+
   // Search engine optimisation
   description: String,
   keywords: String,
 
-  // Site domains, include free subdomain and FQDNs
+  // Site domains list
   domains: [
     {
+      // FQDN, even if mob subdomain (e.g. coruscant.meusitemob.com.br)
       fqdn: String,
-      primary: Boolean
+
+      // All requests will be redirected to the primary domain
+      primary: Boolean,
+
+      // SSL certificates
+      ssl: {
+        enabled: Boolean,
+        certificates: [
+          {
+            issued_by: String,
+            issued_time: Date,
+            expiration_time: Date,
+            path: String,
+            chksum: String
+          }
+        ]
+      },
+
+      // Date events
+      created_time: Date,
+      updated_time: Date,
+
+      // Keep track of DNS records to popup misconfiguration warnings
+      records: [
+        {
+          type: String,
+          name: String,
+          value: String
+        }
+      ]
     }
   ]
 })
