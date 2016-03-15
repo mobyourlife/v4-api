c1d6433dcbfb35b42b67cf5afb9decd2b75896d7
Filipe Oliveira <contato@fmoliveira.com.br>
2016-03-14 22:33:56 -0300

    Setup Restify with logging.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 'use strict'
+
+// Load settings
+const info = require('./package')
+const port = process.env.PORT || 3000
+
+// Load modules
+const restify = require('restify')
+const bunyan = require('bunyan')
+
+// Setup logging
+const log = bunyan.createLogger({
+  name: info.name
+})
+
+// Setup Restify parameters
+var server = restify.createServer({
+  name: info.name,
+  version: info.version,
+  log :log
+})
+
+// Setup routing
+server.get('/hello', (req, res, next) => {
+  res.send({ message: 'It works!' })
+  next()
+})
+
+// Start listening Restify
+server.listen(port, () => {
+  log.info(`${server.name} listening at ${server.url}`)
+})
