2e28db5fc0a7ad91c9759ab343cfce21cd12ee70
Filipe Oliveira <contato@fmoliveira.com.br>
2016-03-17 23:32:13 -0300

    Tweaked code a little bit more.

--- controllers/users.js
+++ controllers/users.js
@@ -1,15 +1,17 @@
 'use strict'
 
+// Load users entity
 const Users = require('../entities/users')
 
+// Exported controllers
 module.exports = {
   index: Index
 }
 
 // List all registered users
 function Index (req, res, next) {
-  let success = (users) => res.send(users)
-  let failure = (err) => res.status(500).send(err)
+  const success = (data) => res.send(data)
+  const failure = (err) => res.status(500).send(err)
 
   Users.list()
     .then(success)
